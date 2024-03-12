a2a8c5d18ab04cd58929151cdeac8fa0b256a67a
Mahektek123 <122737801+Mahektek123@users.noreply.github.com>
2024-03-12 21:25:01 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['source.unsplash.com','images.ctfassets.net'],
+    }
+};
+
+export default nextConfig;
