dc44ec8889402e8f094e14d1eb0b87ce0a4d2bd8
Mahektek123 <122737801+Mahektek123@users.noreply.github.com>
2024-03-21 23:08:41 +0530

    Update Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,59 +1,104 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Image from "next/image";
 import Link from 'next/link';
 import { Entry, EntrySkeletonType, createClient } from 'contentful';
 
+interface HtmlContent {
+  items?: MainContent[]
+}
+interface MainContent {
+  fields?: ContentData;
+  // fields2?: contentForImage;
+}
+interface ContentData {
+  heading?: string;
+  description?: string;
+  img?: string;
+}
+interface ContentData {
+  image?: imgPart;
+}
+
+interface imgPart {
+  fields?: imgField;
+}
+
+interface imgField {
+  file?: imgLoc;
+}
+interface imgLoc {
+  url?: string;
+}
+
+
+
+interface HtmlContent2 {
+  items?: MainContent2[]
+}
+interface MainContent2 {
+  fields?: ContentData2;
+}
+
+interface ContentData2 {
+  buttonText?: string;
+  link?: string;
+}
 
 const Card = () => {
+  const [htmlData, setHtmlData] = useState<HtmlContent>();
+  const [htmlData2, setHtmlData2] = useState<HtmlContent2>();
 
-    var client = createClient({
-        space: "kp0x19lqk3lm",
-        accessToken: "4ZJWSRN5SbOdXvqh3efQAqWyVzpKFmCoWVA3hQlMit0"
-      });
-    
-      let heading = "Pawsitively Adorable: Meet Our Furry Friends!"
-      let img = "https://images.ctfassets.net/kp0x19lqk3lm/7q21jWQJkxFzLtQYEbUcoT/faa9af0ac25f49ae636bf43b3ddb14cf/dog-bg.jpg"
-      let description = "Explore a world of cuteness with our Pawsitively Adorable dog card collection. Each card features charming pups that are ready to bring joy and smiles to your day. From fluffy furballs to playful pals, these cards are a celebration of the pure, unconditional love that dogs bring into our lives."
+  var client = createClient({
+    space: "kp0x19lqk3lm",
+    accessToken: "4ZJWSRN5SbOdXvqh3efQAqWyVzpKFmCoWVA3hQlMit0"
+  });
 
-      const fetchData = async () => {
-        try {
-          const dataEntry = await client.getEntries({ content_type: 'card' })
-          return dataEntry
-            // .then((e) => {
-            //     let heading = document.getElementById("heading")
-            //     if(heading){
-            //         heading!.innerHTML = e.items[0].fields.heading
-            //     }
-            //   document.getElementById("description").innerHTML = e.items[0].fields.description
-            //   document.getElementById('img').setAttribute('src', e.items[0].fields.image.fields.file.url)
-            //   document.getElementById('img').removeAttribute('srcset')
-            // })
-    
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
+  const fetchData = async (): Promise<HtmlContent> => {
+    const dataEntry = (await client.getEntries({ content_type: 'card' }))
+    return dataEntry
+  };
 
-      fetchData().then((e)=> e?.items[0].fields.heading)
+  const fetchData2 = async (): Promise<HtmlContent2> => {
+    const dataEntry2 = (await client.getEntries({ content_type: 'button' }))
+    return dataEntry2
+  };
 
-    return (
-        <>
-            <h2 id='heading' className='font-bold xl:text-xl text-center my-2 md:text-lg lg:text-lg sm:text-sm'>{heading}</h2>
-            <Image className="m-3 w-2/3 rounded-xl"
+  useEffect(() => {
+    fetchData()
+      .then((res) => { setHtmlData(res); })
+
+    fetchData2()
+      .then((res) => { setHtmlData2(res); })
+  }, []);
+
+  return (
+    <>
+      {
+        htmlData && htmlData2 &&(
+          htmlData.items && htmlData2.items && (
+            <>
+              <h2 id='heading' className='font-bold xl:text-xl text-center my-2 md:text-lg lg:text-lg sm:text-sm'>
+                {htmlData.items[0]?.fields?.heading}
+              </h2>
+              <Image className="m-3 w-2/3 rounded-xl"
                 id='img'
-                src={img}
+                src={"https:" + (htmlData.items[0].fields?.image?.fields?.file?.url)?.toString()}
                 alt={"This is img"}
                 height={400}
                 width={400}
                 priority
-            ></Image>
-            <p className='m-9 text-center' id='description'>{description}</p>
+              ></Image>
+              <p className='m-9 text-center' id='description'>
+                {htmlData.items[0]?.fields?.description}
+              </p>
 
-            <Link rel='preload' id='link' className='mb-3 py-2 px-5 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-75' href='/users2'>Click Me</Link>
-
-
-        </>
-    )
+              <Link rel='preload' id='link' className='mb-3 py-2 px-5 bg-blue-500 text-white font-semibold rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-75' href={"" + htmlData2.items[0]?.fields?.link}>{htmlData2.items[0]?.fields?.buttonText}</Link>
+            </>
+          )
+        )
+      }
+    </>
+  )
 }
 
 export default Card
